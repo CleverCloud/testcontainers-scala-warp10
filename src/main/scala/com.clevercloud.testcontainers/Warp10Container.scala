40062a622bd8103f64a439c7ce65ad79022e54dc
Julien Durillon <julien.durillon@clever-cloud.com>
2021-09-01 17:25:15 +0200

    Release testcontainers-scala-warp10 0.39.6.
    - Bump test-containers-scala 0.39.6
    - Implement adding macros to container.

--- src/main/scala/com.clevercloud.testcontainers/Warp10Container.scala
+++ src/main/scala/com.clevercloud.testcontainers/Warp10Container.scala
@@ -4,18 +4,28 @@ import com.clevercloud.testcontainers.warp10.{Warp10Container => JavaWarp10Conta
 import org.testcontainers.utility.DockerImageName
 import com.dimafeng.testcontainers._
 
+import java.io.File
+
 case class Warp10Container(
   tag: String,
+  macrosFolder: Option[File] = None,
 ) extends SingleContainer[JavaWarp10Container] {
 
-  override val container: JavaWarp10Container = new JavaWarp10Container(tag)
+  override val container: JavaWarp10Container = macrosFolder match {
+    case None    => new JavaWarp10Container(tag)
+    case Some(f) => new JavaWarp10Container(tag, f)
+  }
 
   def httpHost: String = container.getHTTPHost
+
   def httpPort: Int = container.getHTTPPort()
+
   def httpHostAddress: String = container.getHTTPHostAddress
+
   def protocol: String = container.getProtocol
 
   def readToken: String = container.getReadToken
+
   def writeToken: String = container.getWriteToken
 }
 
@@ -29,6 +39,10 @@ object Warp10Container {
 
     override def createContainer(): Warp10Container = {
       new Warp10Container(tag)
+    }
+
+    def createWithMacros(macrosFolder: File): Warp10Container = {
+      new Warp10Container(tag, Some(macrosFolder))
     }
 
   }
